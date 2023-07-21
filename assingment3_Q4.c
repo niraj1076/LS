da2e498a84c7d4fa6bcd257e62beb89f17369efe
niraj1076 <81097516+niraj1076@users.noreply.github.com>
2023-07-21 20:55:09 +0530

    Update assingment3_Q4.c

--- assingment3_Q4.c
+++ assingment3_Q4.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
     DIR *dp = NULL;
     struct dirent *entry = NULL;
-    char name[50];
+    char Name[50];
     struct stat sobj;
     char DirName[40];
 
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
 
     while((entry = readdir(dp)) != NULL)
     {
-        sprintf(name,"%s/%s", DirName, entry -> d_name);
-        stat(name, &sobj);
+        sprintf(Name,"%s/%s", DirName, entry -> d_name);
+        stat(Name, &sobj);
         if(S_ISREG(sobj.st_mode))
         {
             if(sobj.st_size == 0)
             {
-                remove(name);
+                remove(Name);
             }
         }   
     }
